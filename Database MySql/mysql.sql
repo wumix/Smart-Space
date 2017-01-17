89e996de737c08e4633e7d381b7ab8c916355761
wumix <florian.foedtke@gmail.com>
2017-01-17 07:35:41 +0100

    SQL Datei für die Errungenschaften
    Wichtig diese Datei muss nur ausgeführt werden wenn ihr den Mod in ein bestehendes Spiel einpflegt.

---
+++ Database MySql/mysql.sql
@@ -0,0 +1,12 @@
+ALTER TABLE `uni1_users` ADD `achievements_mine` INT( 11 ) NOT NULL DEFAULT '1',
+ADD `achievements_research` INT( 11 ) NOT NULL DEFAULT '1',
+ADD `achievements_battle` INT( 11 ) NOT NULL DEFAULT '1',
+ADD `achievements_ship` INT( 11 ) NOT NULL DEFAULT '1',
+ADD `achievements_defence` INT( 11 ) NOT NULL DEFAULT '1',
+ADD `achievements_storage` INT( 11 ) NOT NULL DEFAULT '1',
+ADD `achievements_moon` INT( 11 ) NOT NULL DEFAULT '1',
+ADD `achievements_colony` INT( 11 ) NOT NULL DEFAULT '1',
+ADD `achievements_friend` INT( 11 ) NOT NULL DEFAULT '1',
+ADD `achievements_statpoints` INT( 11 ) NOT NULL DEFAULT '1',
+ADD `achievements_destroy` INT( 11 ) NOT NULL DEFAULT '1',
+ADD `achievements_debris` INT( 11 ) NOT NULL DEFAULT '1'
